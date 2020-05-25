05cc10eb8a45f03a078cce298a8cfe8b136c5b9f
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-05-25 07:58:25 -0700

    Consolidate CSS files

--- src/Components/TaskCard/TaskCard.js
+++ src/Components/TaskCard/TaskCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './TaskCard.css';
 
 export default class TaskCard extends Component {
@@ -11,34 +12,38 @@ export default class TaskCard extends Component {
         let color;
         switch(status) {
             case "inProgress":
-                return ({background: 'rgba(151, 255, 248, 0.3)'});
+                color= 'var(--productify-turquoise)';
+                break;
             case "inReview":
-                color= 'rgba(255, 231, 97, 0.3)';
+                color= 'var(--productify-yellow)';
                 break;
             case "complete":
-                color= 'rgba(168, 247, 148, 0.3)';
+                color= 'var(--productify-green)';
                 break;
             case "backlog":
-                color= 'rgba(255, 157, 157, 0.3)';
+                color= 'var(--productify-red)';
                 break;
             default:
                 color= '#FFF';
           }
           return {background: color};
     }
+
     render() {
         const { id, name, deadline, status} = this.props.task;
         return (
-            <div key={name} 
-            value={id}
-            draggable 
-            onDragStart = {(e) => this.onDragStart(e, id)}
-            className="taskCard" 
-            title={name} 
-            style={this.getColor(status)}>
-                <h4 className="taskCard__name">{name}</h4>
-                <h5 className="taskCard__deadline">{deadline}</h5>
-            </div>
+            <NavLink to="/" className="taskCard__nav">
+                <div key={name} 
+                value={id}
+                draggable 
+                onDragStart = {(e) => this.onDragStart(e, id)}
+                className="taskCard" 
+                title={name} 
+                style={this.getColor(status)}>
+                    <h4 className="taskCard__name">{name}</h4>
+                    <h5 className="taskCard__deadline">{deadline}</h5>
+                </div>
+            </NavLink>
         )
     }
 }
