605a12613f58299bc33034870db427e01582b418
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-05-29 11:16:02 -0700

    Make landing page responsive

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -8,9 +8,9 @@ export default class LandingPage extends Component {
         return (
             <div className="landingPage centeredContent">
                 <p>meet</p>
-                <h1 className="landingPage_title productifyGradient">PRODUCTIFY</h1>
-                <p>your new favorite tool for organizing projects and increasing productivity</p>
-                <NavLink to="/about" className="button landingPage_button">LEARN MORE</NavLink>
+                <h1 className="landingPage__title productifyGradient">PRODUCTIFY</h1>
+                <p className="landingPage__description">your new favorite tool for organizing projects and increasing productivity</p>
+                <NavLink to="/about" className="button landingPage__button">LEARN MORE</NavLink>
             </div>
         )
     }
