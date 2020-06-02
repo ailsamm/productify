981e0ccf33d639f420412cf0bdbba84efb00a0f5
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-06-01 20:21:34 -0700

    Add sign up test

--- src/Components/SignUpPage/SignUpPage.test.js
+++ src/Components/SignUpPage/SignUpPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SignUpPage from './SignUpPage';
 
-it.skip('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SignUpPage/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    serverUrl: "https://peaceful-escarpment-33332.herokuapp.com/api"
+    //serverUrl: "https://peaceful-escarpment-33332.herokuapp.com/api",
+    serverUrl: "http://localhost:8000/api"
 }
 
 export default CONFIG;
