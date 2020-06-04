f419faf4b054b05fa43ed9d22e237f8ad95a7b52
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-06-03 18:12:27 -0700

    Add config comment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     serverUrl: "https://peaceful-escarpment-33332.herokuapp.com/api",
-    env: "DEMO"
+    env: "DEMO" // should either be "DEMO" or "PROD"
 }
 
 export default CONFIG;
